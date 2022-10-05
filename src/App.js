b08554023ea31518e714e7b23cf79e10a120c676
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2022-10-05 10:21:55 +0100

    message

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Error from './pages/Error';
+
+
+
+function App(){
+  return <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='Projects' element={<Projects />} />
+      <Route path='*' element={<Error />} />
+    </Routes>
+  </BrowserRouter>
+}
+
+
+
+export default App;
